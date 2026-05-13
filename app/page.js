4805fa1f03ce796467b81e3ab035e06ce1f0154e
Loc Nguyen <loc611205@gmail.com>
2026-05-13 10:28:58 +0700

    update header and Banner

--- app/page.js
+++ app/page.js
@@ -1,66 +1,82 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.css";
 
 export default function Home() {
+  const header_a = [
+    {
+      id: 1,
+      title: "Home",
+    },
+    {
+      id: 2,
+      title: "Services",
+    },
+    {
+      id: 3,
+      title: "Feature",
+    },
+    {
+      id: 4,
+      title: "Product",
+    },
+    {
+      id: 5,
+      title: "Testimonial",
+    },
+    {
+      id: 6,
+      title: "FAQ",
+    },
+  ];
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <main>
+      <header>
+        <div className="header-inter">
+          <a href="" className="logo">
+            <img src="./images/logo.png" alt="Nexcent logo" />
+          </a>
+
+          <nav>
+            {header_a.map(function (item) {
+              return (
+                <a key={item.id} href="#">
+                  {item.title}
+                </a>
+              );
+            })}
+          </nav>
+
+          <div className="action-group">
+            <a href="" className="logo-link">
+              Login
+            </a>
+
+            <a href="" className="signup-btn">
+              Signup
+            </a>
+          </div>
+        </div>
+      </header>
+      {/* <!-- Banner Section --> */}
+      <section className="Banner">
+        <div className="hero-frameone">
+          <h1 className="lessons">Lessons and insights</h1>
+
+          <h1 className="years">from 8 years</h1>
+
+          <p className="text">
+            Where to grow your business as a photographer: site or social media?
           </p>
+
+          <button className="register">Register</button>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="hero-img">
+          <img src="./images/Illustration.png" alt="Illustration Logo" />
         </div>
-      </main>
-    </div>
+      </section>
+      {/* <!-- Banner Section --> */}
+    </main>
   );
 }
